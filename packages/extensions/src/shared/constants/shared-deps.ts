2b1a49735f103a1e318d690e0a2ce88c5a761d02
Aurel Sziebert <sziebert.au@gmail.com>
2026-08-27 16:37:09 +0200

    refactor(extensions): remove the sandbox runtime

--- packages/extensions/src/shared/constants/shared-deps.ts
+++ packages/extensions/src/shared/constants/shared-deps.ts
@@ -5,8 +5,5 @@
  */
 export const APP_SHARED_DEPS = ['@directus/extensions-sdk', 'vue', 'vue-router', 'vue-i18n', 'pinia'];
 
-/**
- * Dependencies that we guarantee are available in the node_modules of the API when API extensions
- * are used. The `directus:*` extensions are virtual entrypoints available in the sandbox
- */
-export const API_SHARED_DEPS = ['directus', 'directus:api'];
+/** Dependencies that we guarantee are available in the node_modules of the API when API extensions are used. */
+export const API_SHARED_DEPS = ['directus'];
